46134a23217d2014e7f939a2c1873b3cb4257758
Thibaut Durand <durand.tibo@gmail.com>
2026-05-14 10:55:37 -0700

    Fix tests (#12)

--- tests/integration/utils/test_logging.py
+++ tests/integration/utils/test_logging.py
@@ -1,18 +1,18 @@
 from __future__ import annotations
 
 from mlev.testing.fixtures import rich_available, rich_not_available
-from mlev.utils.logging import log_markdown
+from mlev.utils.logging import log_dict_pretty
 
-##################################
-#     Tests for log_markdown     #
-##################################
+#####################################
+#     Tests for log_dict_pretty     #
+#####################################
 
 
 @rich_available
-def test_log_markdown_with_rich() -> None:
-    log_markdown("# Hello")
+def test_log_dict_pretty_with_rich() -> None:
+    log_dict_pretty({"hello": "world"})
 
 
 @rich_not_available
-def test_log_markdown_without_rich() -> None:
-    log_markdown("# Hello")
+def test_log_dict_pretty_without_rich() -> None:
+    log_dict_pretty({"hello": "world"})
