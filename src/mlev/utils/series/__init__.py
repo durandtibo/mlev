fd84fe2c9e857b9d735eaa720138fb40eba825b2
Thibaut Durand <durand.tibo@gmail.com>
2026-05-15 13:12:22 -0700

    Add `series_contains_missing`

---
+++ src/mlev/utils/series/__init__.py
@@ -0,0 +1,7 @@
+r"""Helpers to validate and convert series-like inputs."""
+
+from __future__ import annotations
+
+__all__ = ["series_contains_missing"]
+
+from mlev.utils.series.missing import series_contains_missing
