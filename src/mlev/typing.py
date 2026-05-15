ae3d9afb0ab8d36c71e54cf5ca1c90965c378d94
Thibaut Durand <durand.tibo@gmail.com>
2026-05-15 10:50:04 -0700

    Add `MissingPolicy`

--- src/mlev/typing.py
+++ src/mlev/typing.py
@@ -4,8 +4,9 @@ from __future__ import annotations
 
 __all__ = ["ArrayLike"]
 
+from typing import Any
 
 import numpy as np
 import polars as pl
 
-ArrayLike = np.ndarray | pl.Series | list | tuple
+ArrayLike = np.ndarray | pl.Series | list[Any] | tuple[Any, ...]
